2294064696c46d6b5ef2ae22733554b468456bad
sergheic <serghei.catraniuc.con@hotstar.com>
2022-05-24 15:12:23 +0300

    adopt form ui to scroll and avoid keyboard

--- src/screens/FormScreen.tsx
+++ src/screens/FormScreen.tsx
@@ -4,12 +4,15 @@ import {
     Text,
     View,
     SafeAreaView,
+    TouchableWithoutFeedback,
+    Keyboard,
 } from "react-native";
 import { Colors, SVGs } from "../constants";
 import { InputOutline } from "../components/InputOutline";
 import Spacer from "../components/Spacer";
 import Button from "../components/Button";
 import SvgUri from 'react-native-svg-uri';
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
 // import { Icon } from "../components/Icon";
 // import images from "../constants/images";
 // import common from "../styles/common";
@@ -26,59 +29,65 @@ const FormScreen = () => {
 
     return (
         <View style={styles.main}>
-            <View style={{
-                flex: 1
+            <TouchableWithoutFeedback onPress={Keyboard.dismiss} style={{ flex: 1 }}>
+                <KeyboardAwareScrollView style={{
+                    flex: 1,
+                    padding: 20,
+                }}>
+                    <Text style={styles.headerTitle}>{'Calculate your LVR'}</Text>
+                    <Spacer orientation={'vertical'} space={25} />
+                    <Text style={styles.headerSubtitle}>{'Enter the values below to calculate your loan to value ratio.'}</Text>
+                    <Spacer orientation={'vertical'} space={25} />
+                    <InputOutline
+                        ref={propertyValueRef}
+                        onSubmitEditing={(_) => {
+                            borrowRef.current?.focus();
+                        }}
+                        //   style={common.input}
+                        placeholder={'Estimated property value'}
+                        secondaryPlaceholder={'Value'}
+                        value={''}
+                        leadingIcon={dollarComponent}
+                        keyboardType={'number-pad'}
+                        autoCorrect={false}
+                        autoCapitalize={'none'}
+                        blurOnSubmit={false}
+                        error={undefined}
+                        onChangeText={(value) => {
+                            // onChange(value);
+                            // if (onEmailChange) onEmailChange(value);
+                        }}
+                    //   onBlur={onBlur}
+                    />
+                    <Spacer orientation={'vertical'} space={25} />
+                    <InputOutline
+                        ref={borrowRef}
+                        //   onSubmitEditing={(_) => {
+                        //     borrowRef.current?.focus();
+                        //   }}
+                        //   style={common.input}
+                        placeholder={'Borrow amount'}
+                        secondaryPlaceholder={'Value'}
+                        value={''}
+                        leadingIcon={dollarComponent}
+                        keyboardType={'number-pad'}
+                        autoCorrect={false}
+                        autoCapitalize={'none'}
+                        blurOnSubmit={false}
+                        error={undefined}
+                        onChangeText={(value) => {
+                            // onChange(value);
+                            // if (onEmailChange) onEmailChange(value);
+                        }}
+                    //   onBlur={onBlur}
+                    />
+                    <Spacer orientation={'vertical'} space={50} />
+                </KeyboardAwareScrollView>
+            </TouchableWithoutFeedback>
+            <SafeAreaView style={{
+                marginHorizontal: 20,
+                marginBottom: 20
             }}>
-                <Text style={styles.headerTitle}>{'Calculate your LVR'}</Text>
-                <Spacer orientation={'vertical'} space={25} />
-                <Text style={styles.headerSubtitle}>{'Enter the values below to calculate your loan to value ratio.'}</Text>
-                <Spacer orientation={'vertical'} space={25} />
-                <InputOutline
-                    ref={propertyValueRef}
-                    onSubmitEditing={(_) => {
-                        borrowRef.current?.focus();
-                    }}
-                    //   style={common.input}
-                    placeholder={'Estimated property value'}
-                    secondaryPlaceholder={'Value'}
-                    value={''}
-                    leadingIcon={dollarComponent}
-                    keyboardType={'number-pad'}
-                    autoCorrect={false}
-                    autoCapitalize={'none'}
-                    blurOnSubmit={false}
-                    error={undefined}
-                    onChangeText={(value) => {
-                        // onChange(value);
-                        // if (onEmailChange) onEmailChange(value);
-                    }}
-                //   onBlur={onBlur}
-                />
-                <Spacer orientation={'vertical'} space={25} />
-                <InputOutline
-                    ref={borrowRef}
-                    //   onSubmitEditing={(_) => {
-                    //     borrowRef.current?.focus();
-                    //   }}
-                    //   style={common.input}
-                    placeholder={'Borrow amount'}
-                    secondaryPlaceholder={'Value'}
-                    value={''}
-                    leadingIcon={dollarComponent}
-                    keyboardType={'number-pad'}
-                    autoCorrect={false}
-                    autoCapitalize={'none'}
-                    blurOnSubmit={false}
-                    error={undefined}
-                    onChangeText={(value) => {
-                        // onChange(value);
-                        // if (onEmailChange) onEmailChange(value);
-                    }}
-                //   onBlur={onBlur}
-                />
-                <Spacer orientation={'vertical'} space={50} />
-            </View>
-            <SafeAreaView>
                 <Button
                     title={'Calculate'.toUpperCase()}
                     onPress={() => {
@@ -93,7 +102,6 @@ const FormScreen = () => {
 const styles = StyleSheet.create({
     main: {
         flex: 1,
-        padding: 20,
         backgroundColor: 'white'
     },
     headerTitle: {
